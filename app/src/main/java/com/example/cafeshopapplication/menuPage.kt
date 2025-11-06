ca0fccb24cb4f953b7dfa33edaa1363687dd27ac
Login/SignUp <p2755691@my365.dmu.ac.uk>
2025-11-06 17:50:36 +0000

    create menu. string.xml

--- app/src/main/java/com/example/cafeshopapplication/menuPage.kt
+++ app/src/main/java/com/example/cafeshopapplication/menuPage.kt
@@ -7,6 +7,6 @@ class menuPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menu_page) // <-- make sure the XML name matches
+        setContentView(R.layout.menu_page)
     }
 }
